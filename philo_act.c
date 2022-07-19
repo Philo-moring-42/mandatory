6d50d9d7d678210eed672ac48dad208ac7308ac6
ssolpang <iless3377@naver.com>
2022-07-19 10:32:42 +0900

    philo_eating_test

--- philo_act.c
+++ philo_act.c
@@ -6,32 +6,55 @@
 
 void	philo_eat(t_rule rule, int tid)
 {
-	usleep(rule.time_to_eat);
 	printf("%d is eating now\n", tid + 1);
+	usleep(rule.time_to_eat);
 }
 
+void	philo_sleep(t_rule rule, int tid)
+{
+	printf("%d is sleeping now\n", tid + 1);
+	usleep(rule.time_to_sleep);
+}
+
+void	philo_think(int tid)
+{
+	printf("%d is thinking now\n", tid + 1);
+	usleep(200);
+}
 
 void	*philo_act(void *data)
 {
 	int		tid;
 	t_philo	*philo;
+	int		i;
 
 	philo = (t_philo *)data;
 	tid = philo->tid_index;
-	if (tid % 2 == 0)
+	i = 0;
+	while (i++ < 5)
 	{
-		pthread_mutex_lock(philo->left_fork);
-		printf("%d get left fork\n" , tid + 1);
-		pthread_mutex_lock(philo->right_fork);
-		printf("%d get right fork\n" , tid + 1);
+		if (tid % 2 == 0)
+		{
+			pthread_mutex_lock(philo->left_fork);
+			printf("%d get left fork\n" , tid + 1);
+			pthread_mutex_lock(philo->right_fork);
+			printf("%d get right fork\n" , tid + 1);
+		}
+		else
+		{
+			usleep(2000);
+			pthread_mutex_lock(philo->right_fork);
+			printf("%d get right fork\n" , tid + 1);
+			pthread_mutex_lock(philo->left_fork);
+			printf("%d get left fork\n" , tid + 1);
+		}
+		philo_eat(*(philo->param->rule), philo->tid_index);
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
+		philo_sleep(*(philo->param->rule), philo->tid_index);
+		philo_think(philo->tid_index);
+			//pthread_create
 	}
-	// else
-	// {
-	// 	pthread_mutex_lock(philo->right_fork);
-	// 	pthread_mutex_lock(philo->left_fork);
-	// }
-	philo_eat(*(philo->param->rule), philo->tid_index);
-		//pthread_create
 	return (NULL);
 }
 
@@ -46,16 +69,17 @@ int	philo_run(t_rule *rule)
 	while (i < rule->num_of_philo)
 	{
 		param.philo[i].tid_index = i;
-		pthread_create(&param.tids[i], NULL, philo_act, param.philo);
+		pthread_create(&param.tids[i], NULL, philo_act, &param.philo[i]);
 	//	pthread_join(param.tids[i], NULL);
 		++i;
 	}
+	/* i = 0;
+	while (i < rule->num_of_philo)
+	{
+		test_philo(param.philo[i], i);
+		++i;
+	} */
 	i = 0;
-	// while (i < rule->num_of_philo)
-	// {
-	// 	test_philo(param.philo[i], i);
-	// 	++i;
-	// }
 	while (i < rule->num_of_philo)
 	{
 		pthread_join(param.tids[i], NULL);
