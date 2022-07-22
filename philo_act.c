52433f79867e6a7fd06439b9562cd9ff0b817896
Haeun <gkdms4248@gmail.com>
2022-07-22 18:07:34 +0900

    real complete?

--- philo_act.c
+++ philo_act.c
@@ -6,31 +6,19 @@
 /*   By: hjeong <hjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 13:40:57 by hjeong            #+#    #+#             */
-/*   Updated: 2022/07/22 17:41:23 by hjeong           ###   ########.fr       */
+/*   Updated: 2022/07/22 18:06:30 by hjeong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
 
-static void	odd_philo_eat(t_philo *philo, int tid)
+static void	dining_philo_eat(t_philo *philo, int tid)
 {
 	pthread_mutex_lock(philo->left_fork);
-	print_terminal(philo->param, tid + 1, "has taken a left fork");
+	print_terminal(philo->param, tid + 1, "has taken a fork");
 	pthread_mutex_lock(philo->right_fork);
-	print_terminal(philo->param, tid + 1, "has taken a right fork");
-	philo_eat(philo->param->rule, philo, philo->tid_index);
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
-}
-
-static void	even_philo_eat(t_philo *philo, int tid)
-{
-	usleep(philo->param->rule->time_to_eat * 800);
-	pthread_mutex_lock(philo->right_fork);
-	print_terminal(philo->param, tid + 1, "has taken a right fork");
-	pthread_mutex_lock(philo->left_fork);
-	print_terminal(philo->param, tid + 1, "has taken a left fork");
+	print_terminal(philo->param, tid + 1, "has taken a fork");
 	philo_eat(philo->param->rule, philo, philo->tid_index);
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
@@ -43,12 +31,11 @@ static void	*philo_act(void *data)
 
 	philo = (t_philo *)data;
 	tid = philo->tid_index;
+	if (tid % 2 == 1)
+		usleep(philo->param->rule->time_to_eat * 800);
 	while (philo->param->rule->is_dining == TRUE)
 	{
-		if (tid % 2 == 0)
-			odd_philo_eat(philo, tid);
-		else
-			even_philo_eat(philo, tid);
+		dining_philo_eat(philo, tid);
 		philo->start_starving_time = get_time(philo->param);
 		philo_sleep(philo->param->rule, philo, philo->tid_index);
 		philo_think(philo->param->rule, philo, philo->tid_index);
